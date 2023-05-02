df309f2716449d709495764f1e65fbe37acbd343
Rystambek <rustambekjumaqulov@gmail.com>
2023-05-02 15:45:31 +0500

    flask

--- core/db.py
+++ core/db.py
@@ -4,3 +4,18 @@ from tinydb import TinyDB, Query
 def get_db():
     db = TinyDB('db.json')
     return db.table('grocery').all()
+
+def get_type(type: str) -> list:
+    db = TinyDB('db.json')
+    q = Query()
+    return db.table('grocery').search(q.type == type)
+
+def get_price(price: str) -> list:
+    db = TinyDB('db.json')
+    q = Query()
+    return db.table('grocery').search(q.price == price)
+
+def get_name(name: str) -> list:
+    db = TinyDB('db.json')
+    q = Query()
+    return db.table('grocery').search(q.name == name)
--- core/views.py
+++ core/views.py
@@ -1,13 +1,6 @@
 from core import app
 from flask import request, render_template
-from .db import get_db
-
-@app.route('/<name>', methods=['GET'])
-def index(name):
-
-    context = ['Hello', 'World', 'From', 'Flask', 'App']
-
-    return render_template('home.html', context=context, title='index')
+from .db import get_db,get_type,get_name,get_price
 
 
 @app.route('/grocery', methods=['GET'])
@@ -16,3 +9,30 @@ def grocery():
     context = get_db()
 
     return render_template('grocery.html', context=context, title='grocery')
+
+@app.route('/grocery/type/<type>', methods=['GET'])
+
+def type(type):
+    
+    context = get_type(type)
+
+
+    return render_template('grocery.html', context=context, title=f'{type}')
+
+
+@app.route('/grocery/name/<name>', methods=['GET'])
+def name(name):
+    
+    context = get_name(name)
+
+
+    return render_template('grocery.html', context=context, title=f'{name}')
+
+
+@app.route('/grocery/price/<price>', methods=['GET'])
+def price(price):
+    
+    context = get_price(price)
+
+
+    return render_template('grocery.html', context=context, title=f'{price}')
